3ad61ee3642675d18314dad1171f9626152d2294
juansecorrea12 <juansecorrea12@gmail.com>
2020-07-15 16:05:08 -0500

    get Users from API and list into a table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import Formulario from "./components/formulario.jsx";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://academlo-api-users.herokuapp.com/users")
+      // .then((respuesta) => {
+      //   respuesta.json();
+      // })
+      .then((response) => response.json())
+      .then((result) => this.setState({ users: result.data }))
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    return (
+      <div>
+        <Formulario />
+        <div className="tablas">
+          <h1>Usuarios del sistema</h1>
+          <table className="table header">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Email</th>
+                <th>Password</th>
+              </tr>
+            </thead>
+          </table>
+          {this.state.users.map((user, index) => {
+            return (
+              <table className="table" key={index}>
+                <tbody>
+                  <tr>
+                    <td>{user.name}</td>
+                    <td>{user.lastname}</td>
+                    <td>{user.email}</td>
+                    <td>{user.password}</td>
+                  </tr>
+                </tbody>
+              </table>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
