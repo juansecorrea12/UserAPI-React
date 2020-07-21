ff2bd40c77b3a3ae5ff12b0e90446532480dc93e
juansecorrea12 <juansecorrea12@gmail.com>
2020-07-21 16:49:57 -0500

    build application

---
+++ build/precache-manifest.80f68bf5a469fba59a9d80be4a7b88a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "039e05b0d2a4321ac729bb9fcb5d5dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff1856e641cdc4304c99",
+    "url": "/static/css/main.8d596957.chunk.css"
+  },
+  {
+    "revision": "274c986b9ab095773908",
+    "url": "/static/js/2.c63d7d4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c63d7d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1856e641cdc4304c99",
+    "url": "/static/js/main.8a8c9f92.chunk.js"
+  },
+  {
+    "revision": "5ca27509396fe983371e",
+    "url": "/static/js/runtime-main.364b107f.js"
+  }
+]);
